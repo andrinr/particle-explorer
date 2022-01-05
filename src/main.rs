e24b33d0a0759aacc9e918d975053b65b1d5b862
Andrin Rehmann <andrinrehmann@gmail.com>
2022-01-05 19:45:09 +0100

    add Cell

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate nannou;
 use nannou::prelude::*;
 use nannou::glam::Vec2;
 
+const GRAVITY : f32 = 1000.0;
+const MAX_DEPTH : i32 = 16;
+const PARTICLE_COUNT : usize = 1<<12;
+
 fn main() {
     nannou::app(model)
         .update(update)
@@ -16,15 +20,72 @@ struct Model {
 
 #[derive(Copy, Clone)]
 struct Particle {
-    p : Vec2,
-    v : Vec2
+    position : Vec2,
+    velocity : Vec2
+}
+
+impl Particle {
+    fn acc(self, dt : f32) -> Vec2 {
+        // Random noise
+        let mut acc : Vec2 = Vec2::new(random_f32() - 0.5, random_f32() - 0.5) * 30.0 * dt;
+        // Gravity
+        let d : f32 = self.position.length() + 0.5;
+        acc -= self.position.clone().normalize() / d * GRAVITY * dt;   
+    
+        return acc;
+    }
+
+    fn kick_drift_kick(mut self, dt : f32) {
+        // Leap-Frog Integration
+        // Kick
+        let v_half = self.velocity + self.acc(dt) * dt * 0.5;
+        // Drift
+        self.position += v_half * dt;
+        // Kick
+        self.velocity = v_half + self.acc(dt) * dt * 0.5;
+    }
+
+    fn enforce_boundary_conditions(mut self, width : f32, height : f32) {
+        // Periodic Boundary Conditions
+        self.position.x -= (self.position.x > width * 0.5) as i32 as f32 * width;
+        self.position.x += (self.position.x < -width * 0.5) as i32 as f32 * width;
+        self.position.y -= (self.position.y > height * 0.5) as i32 as f32 * height;
+        self.position.y += (self.position.y < -height * 0.5) as i32 as f32 * height;
+    }
 }
 
 struct Cell {
-    tl : Vec2,
-    br : Vec2,
-    child_a : Box<Cell>,
-    child_b : Box<Cell>,
+    center : Vec2,
+    size : Vec2,
+    depth : i32,
+    start : i32, 
+    end : i32,
+    child_a : Option<Box<Cell>>,
+    child_b : Option<Box<Cell>>,
+}
+
+
+impl Cell {
+    // Split domain
+    fn split(self, mut particles : [Particle; PARTICLE_COUNT]) -> [Particle; PARTICLE_COUNT] {
+        let next_depth = self.depth + 1;
+
+        let dim = (self.size[0] > self.size[1]) as usize;
+
+        let split = self.center[dim];
+
+        let n_left = 0;
+
+        while(n_left - (self.end - self.start) > 1) {
+            n_left += 
+        }
+
+        return particles;
+    }
+}
+
+trait Inner {
+
 }
 
 fn model(app: &App) -> Model {
@@ -36,14 +97,14 @@ fn model(app: &App) -> Model {
 
     let p = Vec2::new(10.0, 0.0);
     let v = Vec2::new(0.0, 0.0);
-    let mut particles : [Particle; 1<<12] = [Particle{p : p, v : v}; 1<<12];
+    let mut particles : [Particle; PARTICLE_COUNT] = [Particle{position : p, velocity : v}; PARTICLE_COUNT];
 
     for (_i, particle) in particles.iter_mut().enumerate() {
-        particle.v.x = 100.0 * (random_f32() - 0.5) * (random_f32() - 0.5);
-        particle.v.y = 100.0 * (random_f32() - 0.5) * (random_f32() - 0.5);
+        particle.velocity.x = 100.0 * (random_f32() - 0.5) * (random_f32() - 0.5);
+        particle.velocity.y = 100.0 * (random_f32() - 0.5) * (random_f32() - 0.5);
 
-        particle.p.x = w * (random_f32() - 0.5);
-        particle.p.y = h * (random_f32() - 0.5);
+        particle.position.x = w * (random_f32() - 0.5);
+        particle.position.y = h * (random_f32() - 0.5);
     }
 
     println!("length: {}", particles.len());
@@ -59,35 +120,11 @@ fn update(app: &App, _model: &mut Model, _update: Update) {
     let win = window.rect();
     let h = win.h();
     let w = win.w();
-    let h_2 = h * 0.5;
-    let w_2 = w * 0.5;
 
     for (_i, particle) in _model.particles.iter_mut().enumerate() {
-        // Leap-Frog Integration
-        // Kick
-        let v_half = particle.v + acc(particle.p, dt) * dt * 0.5;
-        // Drift
-        particle.p += v_half * dt;
-        // Kick
-        particle.v = v_half + acc(particle.p, dt) * dt * 0.5;
-
-        // Periodic Boundary Conditions
-        particle.p.x -= (particle.p.x > w_2) as i32 as f32 * w;
-        particle.p.x += (particle.p.x < -w_2) as i32 as f32 * w;
-        particle.p.y -= (particle.p.y > h_2) as i32 as f32 * h;
-        particle.p.y += (particle.p.y < -h_2) as i32 as f32 * h;
+        particle.kick_drift_kick(dt);
+        particle.enforce_boundary_conditions(w, h);
     }
-}
-
-fn acc(pos : Vec2, dt : f32) -> Vec2 {
-    // Random noise
-    let mut acc : Vec2 = Vec2::new(random_f32() - 0.5, random_f32() - 0.5) * 30.0 * dt;
-    // Gravity
-    let d : f32 = pos.length() + 0.5;
-    const G : f32 = 10000.0;
-    acc -= pos.clone().normalize() / d * G * dt;   
-
-    return acc;
 }
 
 fn view(_app: &App, _model: &Model, frame: Frame) {
@@ -97,7 +134,7 @@ fn view(_app: &App, _model: &Model, frame: Frame) {
     draw.background().color(BLACK);
 
     for particle in _model.particles {
-        draw.ellipse().color(WHITE).x_y(particle.p.x, particle.p.y).radius(1.0);
+        draw.ellipse().color(WHITE).x_y(particle.position.x, particle.position.y).radius(1.0);
     }
 
     // put everything on the frame
