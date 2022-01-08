47634cabaaae246f91eaded1f45c69aa7560a62b
Andrin Rehmann <andrinrehmann@gmail.com>
2022-01-08 17:58:25 +0100

    change to splices, also allows for parallelization

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use nannou::prelude::*;
 use nannou::glam::Vec2;
 
 const GRAVITY : f32 = 1000.0;
-const MAX_DEPTH : i32 = 16;
-const PARTICLE_COUNT : usize = 1<<4;
+const MAX_DEPTH : i32 = 2;
+const PARTICLE_COUNT : usize = 1<<6;
 
 fn main() {
     nannou::app(model)
@@ -14,18 +14,8 @@ fn main() {
         .run();
 }
 
-struct Test {
-    a : i32,
-}
-
-impl Test {
-    fn incr(mut self, b : i32) {
-        self.a += b;
-    }
-}
-
 struct Model {
-    particles : [Particle; PARTICLE_COUNT],
+    particles : [Particle; PARTICLE_COUNT]
 }
 
 #[derive(Copy, Clone)]
@@ -37,23 +27,19 @@ struct Particle {
 impl Particle {
     fn acc(self) -> Vec2 {
         // Random noise
-        let mut acc : Vec2 = Vec2::new(random_f32() - 0.5, random_f32() - 0.5) * 3000.0;
+        let mut acc : Vec2 = Vec2::new(random_f32() - 0.5, random_f32() - 0.5) * 3.0;
         // Gravity
-        //let d : f32 = self.position.length() + 0.5;
-        //acc -= self.position.clone().normalize() / d * GRAVITY * dt;   
+        let d : f32 = self.position.length() + 0.5;
+        acc -= self.position.clone().normalize() / d * GRAVITY;   
         return acc;
     }
 
     fn kick_drift_kick(&mut self, dt : f32) {
         // Leap-Frog Integration
         // Kick
-        println!("pos : {}", self.position);
         let v_half = self.velocity + self.acc() * dt * 0.5;
         // Drift
-        println!("v half : {}", v_half);
         self.position += v_half * dt;
-        println!("pos : {}", self.position);
-        //println!("v half : {}", self.v_half);
         // Kick
         self.velocity = v_half + self.acc() * dt * 0.5;
     }
@@ -71,43 +57,61 @@ struct Cell {
     center : Vec2,
     size : Vec2,
     depth : i32,
-    start_index : i32, 
-    end_index : i32,
     child_a : Option<Box<Cell>>,
     child_b : Option<Box<Cell>>,
 }
 
 impl Cell {
     // Split domain
-    fn split(mut self, mut particles : [Particle; PARTICLE_COUNT]) -> [Particle; PARTICLE_COUNT] {
+    fn split(mut self, mut particles : &mut[Particle]){
         let next_depth = self.depth + 1;
+        let n = particles.len();
 
         if next_depth == MAX_DEPTH {
-            return particles;
+            return;
         }
 
         let dimension = (self.size[0] > self.size[1]) as usize;
-        let half_count : i32  = (self.end_index - self.start_index) / 2;
+        let half_count : i32  = (n as i32) / 2;
         let mut step : f32 = self.size[dimension] / 2.0;
         let mut split : f32 = self.center[dimension];
         
         let mut left_count : i32 = 0;
 
-        // binary search for propper split
-        let particles_slice : &[Particle] = &particles[self.start_index as usize..self.end_index as usize];
+        println!("{},{},{},{}", dimension, half_count, step, split);
+
         loop {
-            for (_i, particle) in particles_slice.iter().enumerate() {
-                left_count += (particle.position[dimension] < split) as i32 + self.start_index;
+            left_count = 0;
+
+            for (_i, particle) in particles.iter().enumerate() {
+                left_count += (particle.position[dimension] < split ) as i32;
             }
+
+            if abs(left_count - half_count) <= 1 { break; }
 
             step /= 2.0;
 
             split += if left_count < half_count { step } else { -step};
-
-            if abs(left_count - half_count) <= 1 { break; }
         }
         
         // TODO: reshuffle array
+        let mut i = n - 1;
+        let mut j = 0;
+        
+        loop {
+            if i == j { break ;}
+
+            if particles[i].position[dimension] < split {
+                i += 1;
+                continue;
+            }
+            if particles[i].position[dimension] > split {
+                j -= 1;
+                continue;
+            }
+
+            particles.swap(i, j);
+        }
 
         // Define new child cells
         let mut center_a : Vec2 = Vec2::new(0.0, 0.0);
@@ -122,27 +126,32 @@ impl Cell {
         let size_a : Vec2 = self.center.clone() - center_a * 2.0;
         let size_b : Vec2 = center_b.clone() - self.center * 2.0;
 
-        self.child_a = Some(Box::new(Cell {
+        let child_a = Box::new(Cell {
             center : center_a,
             size : size_a,
             depth : next_depth,
-            start_index : self.start_index,
-            end_index : self.start_index + left_count,
             child_a : None,
             child_b : None
-        }));
+        });
 
-        self.child_b = Some(Box::new(Cell {
+        child_a.split(&mut particles[0 .. left_count as usize]);
+
+        let child_b = Box::new(Cell {
             center : center_b,
             size : size_b,
             depth : next_depth,
-            start_index : self.start_index + left_count,
-            end_index : self.end_index,
             child_a : None,
             child_b : None
-        }));
+        });
 
-        return particles;
+        child_b.split(&mut particles[left_count as usize .. n]);
+
+        self.child_a = Some(child_a);
+        self.child_b = Some(child_b);
+    }
+
+    fn draw(self, app: &App) {
+        
     }
 }
 
@@ -165,8 +174,6 @@ fn model(app: &App) -> Model {
         particle.position.y = h * (random_f32() - 0.5);
     }
 
-    println!("length: {}", particles.len());
-
     Model {particles : particles}
 }
 
@@ -186,7 +193,16 @@ fn update(app: &App, model: &mut Model, update: Update) {
         particle.kick_drift_kick(dt);
         particle.enforce_boundary_conditions(w, h);
     }
-    //println!("Update {}", dt);
+
+    let root = Cell {
+        center : Vec2::new(w / 2.0, h / 2.0),
+        size : Vec2::new(w, h),
+        depth : 0,
+        child_a : None, 
+        child_b : None
+    };
+
+    root.split(&mut model.particles[0..PARTICLE_COUNT]);
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -196,8 +212,17 @@ fn view(app: &App, model: &Model, frame: Frame) {
     draw.background().color(BLACK);
 
     for particle in model.particles {
-        draw.ellipse().color(WHITE).x_y(particle.position.x, particle.position.y).radius(1.0);
+        draw.ellipse()
+        .color(WHITE)
+        .x_y(particle.position.x, particle.position.y)
+        .radius(1.0)
+        .resolution(10.0);
     }
+
+    let window = app.main_window();
+    let win = window.rect();
+    let h = win.h();
+    let w = win.w();
 
     // put everything on the frame
     draw.to_frame(app, &frame).unwrap();
